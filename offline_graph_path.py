98190af6a2daa055e9e49ba5d1199bd6878c874b
Roey Ogen <eo@campus.technion.ac.il>
2022-08-11 12:41:27 +0300

    thursday 12:41

--- offline_graph_path.py
+++ offline_graph_path.py
@@ -41,7 +41,7 @@ class Node:
                     val = ' Agent '
                     res += " " + colored(str(val).ljust(7), 'blue') + " |"  # format
                 elif val == self.CHARGE:
-                    res += " " + colored(("CHARGE"+"⚡️"[0]).ljust(7), 'green') + " |"  # format
+                    res += " " + colored(("CHARGE").ljust(7), 'green') + " |"  # format
                 else:
                     if val == 0:
                         res += " " + colored("".ljust(7), 'white') + " |"  # format
@@ -126,7 +126,7 @@ class MetaNode:
                             val = 'Agent_{}'.format(i + 1)
                     res += " " + colored(str(val).ljust(7), 'blue') + " |"  # format
                 elif val == self.CHARGE:
-                    res += " " + colored(("CHARGE" + "⚡️"[0]).ljust(7), 'green') + " |"  # format
+                    res += " " + colored(("CHARGE").ljust(7), 'green') + " |"  # format
                 else:
                     if val == 0:
                         res += " " + colored("".ljust(7), 'white') + " |"  # format
